efbc467563070b457416387162d5c3c19223b7c5
Johan2109 <johanronaldolopezmontes@gmail.com>
2025-02-09 04:42:26 -0500

    Add comment

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "./index.css"; // Importar TailwindCSS
+import "./index.css"; // Para Importar TailwindCSS
 
 const app = createApp(App);
 app.use(router);
